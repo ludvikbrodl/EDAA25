67ea18e19d2175918df3e7ea3b7ccd0602acec83
Ludvik Brodl <Ludvik.Brodl@lunicore.se>
2016-09-07 10:50:36 +0200

    working lab1

--- rpn/rpn.c
+++ rpn/rpn.c
@@ -4,21 +4,22 @@
 
 #define N		(10)
 
-bool trailingDigit = false;
+bool trailing_digit = false;
 int digits[8];
-int numberDigits = 0;
+int number_digits = 0;
 int stack[N];
 int i = 0;
 int ch;
 int line = 1;
 
-bool handleTrail();
+bool handle_trail();
+void skip_to_next_line();
 
 int main(void)
 {
 	while ((ch = getchar()) != EOF) {
 		if (ch == '\n') {
-			if (handleTrail()) {
+			if (handle_trail()) {
 				if (i != 1) { //check for empty stack/several ints on newline
 				printf("line %d: error at \\n\n", line);
 				}
@@ -33,14 +34,22 @@ int main(void)
 
 
 		} else if (isdigit(ch)) {
-			digits[numberDigits++] = ch - '0';
-			trailingDigit = true;
+			if (i > 9) {
+				printf("line %d: error at %c\n", line, ch);
+				skip_to_next_line();
+				line++;
+				i = 0;
+			} else {
+				digits[number_digits++] = ch - '0';
+				trailing_digit = true;
+			}
 		} else if (ch == ' ') {
-			handleTrail();
+			handle_trail();
 		} else { //operator
-			handleTrail();
+			handle_trail();
 			if (i < 2) { //not enough ints to perform operator
 				printf("line %d: error at %c\n", line, ch);
+				skip_to_next_line();
 				i = 0;
 				line++;
 			} else if (ch == '+') {
@@ -57,26 +66,32 @@ int main(void)
 				stack[i++] = a * b;
 			} else if (ch == '/') {
 				int a = stack[--i];
-				if (a == 0) {
+				if (a == 0) { //div by 0
 					printf("line %d: error at /\n", line);
+					skip_to_next_line();
 					i = 0;
 					line++;
 				} else {
 					int b = stack[--i];
-					stack[i++] = b / a;	
+					stack[i++] = b / a;
 				}
+			} else { //invalid char
+				printf("line %d: error at %c\n", line, ch);
+				skip_to_next_line();
+				i = 0;
+				line++;
 			}
 		}
 	}
 	return 0;
 }
 
-bool handleTrail() {
-	if (trailingDigit) {
+bool handle_trail() {
+	if (trailing_digit) {
 
 		int itr;
 		int number = 0;
-		for (itr = 0; itr < numberDigits; itr++) {
+		for (itr = 0; itr < number_digits; itr++) {
 			number = (number * 10) + (digits[itr]);
 		}
 		if (i > 9) {
@@ -86,9 +101,17 @@ bool handleTrail() {
 			return false;
 		} else {
 			stack[i++] = number;
-			numberDigits = 0;
-			trailingDigit = false;	
+			number_digits = 0;
+			trailing_digit = false;
 		}
 	}
 	return true;
+}
+
+void skip_to_next_line() {
+	while ((ch = getchar()) != EOF) {
+		if (ch == '\n') {
+			return;
+		}
+	}
 }
