ff5e4d0f8de1ba15621efa3627da40d05a187923
Ludvik Brodl <Ludvik.Brodl@lunicore.se>
2016-09-16 15:45:36 +0200

    remade word to use array with growth of 2x each time too small

--- word/word.c
+++ word/word.c
@@ -1,101 +1,44 @@
 #include <stdio.h>
 #include <ctype.h>
 
-typedef struct list_t list_t;
-
-struct list_t {
-	list_t*	succ;
-	list_t*	pred;
-	char 	data;
-};
-
-list_t* new_list(char data)
-{
-	list_t* list;
-	list = malloc(sizeof(list_t));
-
-	(*list).succ = NULL;
-	list->pred = list;
-	(*list).data = data;
-
-	return list;
-}
-
-void free_list(list_t** list)
-{
-	list_t*		p;
-	list_t*		q;
-
-	p = *list;
-	if (p == NULL) {
-		return;
-	}
-	p->pred->succ = NULL;
-	do {
-		q = p->succ;
-		free(p);
-		p = q;
-	} while (p != NULL);
-	*list = NULL; //dunno why
-}
-
-void insert_last(list_t** list, char data)
-{
-	list_t*		p = *list;
-	list_t*		q;
-
-	if (p == NULL) {
-		*list = new_list(data);
-		return;
-	}
-
-	q = malloc(sizeof(list_t));
-
-	q->data 		= data;
-	q->pred 		= p->pred;
-	q->succ 		= p;
-	p->pred->succ 	= q;
-	p->pred 		= q;
-}
-
-int ch;
-
-
-
 
 int main(void)
 {
-	int length, length_temp;
-	list_t* head;
-	list_t* head_temp;
-	head = NULL;
-	head_temp = NULL;
-	length = 0;
-	length_temp = 0;
+	int ch;
+	char* str;
+	char* str_temp;
+	int index, index_temp, max;
+	index = 0;
+	index_temp = 0;
+	max 		= 10;
+	str 		= calloc(max, sizeof(char));
+	str_temp 	= calloc(max, sizeof(char));
+
 	while ((ch = getchar()) != EOF) {
-		free_list(&head_temp);
 		while (isalpha(ch)) {
-			insert_last(&head_temp, ch);
-			length_temp++;
+			if (index_temp > max) {
+				max = max * 2;
+				str_temp = realloc(str_temp, sizeof(char)*max);
+			}
+			str_temp[index_temp++] = ch;
 			ch = getchar();
 		}
-		if (length_temp > length) {
-			free_list(&head);
-			head = head_temp;
-			length = length_temp;
-			head_temp = NULL; //'make room' for new list
-		} else {
-			free_list(&head_temp);
+		if (index_temp > index) {
+			index = index_temp;
+			free(str);
+			str = str_temp;
+			str_temp = calloc(max, sizeof(char));
 		}
-		length_temp = 0;
+		index_temp = 0;
 	}
-	printf("%d characters in longest word: ", length);
-	list_t* p = head;
-	do {
-		printf("%c", p->data);
-		p = p->succ;
-	} while (p != head);
+	printf("%d characters in longest word: ", index);
+	int i = 0;
+	while (i < index) {
+		printf("%c", str[i++]);
+	}
 	printf("\n");
-	free_list(&head);
+	free(str);
+	free(str_temp);
+	
 	return 0;
 }
