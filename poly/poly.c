e4c78c6a264b8826f7d0366343813de3e3c77f8d
Ludvik Brodl <ludvik.brodl@gmail.com>
2016-09-20 20:42:20 +0200

    ass3 passed, far from optimized code, went for readability

--- poly/poly.c
+++ poly/poly.c
@@ -81,42 +81,62 @@ void insert_last(list_t** list, term_t* term)
 	p->pred->succ 	= q;
 	p->pred			= q;
 }
-// - 2x^2 
+
 term_t* new_term(const char* str)
-{	
+{
 	term_t* term = malloc(sizeof(term_t));
-	term->coff = 1;
-	if (str[0] == '-') {
-		term->coff = -1;
-		str++;
-		str++;
-	} else if(str[0] == '+') {
-		str++;
-		str++;
-	}
+	//printf("\n%s\n", str);
+	const char* temp = str;
 	char coff_str[100];
 	char exp_str[100];
+	memset(coff_str, '\0', sizeof(coff_str));
+	memset(exp_str, '\0', sizeof(exp_str));
 
-	char* x = strchr(str, 'x');
-	if (x != NULL && (x-str) == 0) {
+	if (str[0] == '-') {
+		term->coff = -1;
+	} else {
 		term->coff = 1;
-	} else if ((x-str) == 0) {
-		memcpy(coff_str, str, x-str+1);
-		term->coff = term->coff * atoi(coff_str);
-	} else {
-		term->coff = term->coff * atoi(str);
 	}
-	
+	char* x = strchr(str, 'x');
+	if (x != NULL) {
+		if (str[0] == 'x' && str[1] == ' ') {
+			coff_str[0] = '1';
+			coff_str[1] = '\0';
+		} else if (str[1] == ' ') {
+			temp++;
+			memcpy(coff_str, temp, x-(&str[1]));
+		} else if (x == str){
+			coff_str[0] = '1';
+			coff_str[1] = '\0';
+		} else if (&str[1] == x && str[0] == '-') {
+			coff_str[0] = '1';
+			coff_str[1] = '\0';
+		} else if (&str[1] != x && str[0] == '-') {
+			term->coff = 1;
+			memcpy(coff_str, temp, x-(&str[0]));
+		} else {
+			memcpy(coff_str, temp, x-(&str[0]));
+		}
+	} else {
+		memcpy(coff_str, temp+2, strlen(temp)-2);
+	}
+
+	term->coff = term->coff * atoi(coff_str);
+	//printf("digit %i \t string %s\n", term->coff, coff_str);
 	char* e = strchr(str, '^');
+	char* end = strchr(str, '\0');
 	if (e != NULL) {
-		strncpy(exp_str, x+2, strlen(str)*sizeof(char)-(x-str));
-		term->exp = atoi(exp_str);
+		memcpy(exp_str, e+1, end-e);
 	} else if (x != NULL) {
-		term->exp = 1;
+		exp_str[0] = '1';
+		exp_str[1] = '\0';
 	} else {
-		term->exp = 0;
+		exp_str[0] = '0';
+		exp_str[1] = '\0';
 	}
+	term->exp = atoi(exp_str);
 	return term;
+
 }
 
 term_t* new_term2(int coff, int exp) {
@@ -197,7 +217,9 @@ poly_t* mul(poly_t* a, poly_t* b)
 	poly_t* product = malloc(sizeof(poly_t));
 	do {
 		do {
-			term_t* t = new_term2(alist->term->coff * blist->term->coff, alist->term->exp + blist->term->exp);
+			int coff = alist->term->coff * blist->term->coff;
+			int exp = alist->term->exp + blist->term->exp;
+			term_t* t = new_term2(coff, exp);
 			insert(&prod, t);
 			blist = blist->succ;
 		} while (blist != b->terms);
@@ -205,27 +227,44 @@ poly_t* mul(poly_t* a, poly_t* b)
 	} while (alist != a->terms);
 	product->terms = prod;
 	//check for collisions on exponents
-	//pointer-fun!	
+	//pointer-fun!
 	list_t* prod_list1 = prod;
 	list_t* prod_list2 = prod;
 	do {
 		do {
 			list_t* temp = prod_list2->succ;
-			if(prod_list1 != prod_list2) {
-				if(prod_list1->term->exp == prod_list2->term->exp) {
-				prod_list1->term->coff += prod_list2->term->coff;
-				prod_list2->pred->succ = prod_list2->succ;
-				prod_list2->succ->pred = prod_list2->pred;
-				prod_list2->succ = NULL;
-				prod_list2->pred = prod_list2;
-				free_list(&prod_list2);				
+			if (prod_list1 != prod_list2) {
+				if (prod_list1->term->exp == prod_list2->term->exp) {
+					prod_list1->term->coff += prod_list2->term->coff;
+					prod_list2->pred->succ = prod_list2->succ;
+					prod_list2->succ->pred = prod_list2->pred;
+					prod_list2->succ = NULL;
+					prod_list2->pred = prod_list2;
+					free_list(&prod_list2);
 				}
 			}
 			prod_list2 = temp;
 		} while (prod_list2 != prod);
 		prod_list1 = prod_list1->succ;
 	} while (prod_list1 != prod);
-	
+	//sort list the brute force way, cba doing quick sort.
+	//hurray
+	list_t* p = prod;
+	do {
+		if (p->term->exp > p->succ->term->exp) {
+			p = p->succ;
+		} else {
+			list_t* q = p->succ;
+			p->pred->succ	= q;
+			q->pred			= p->pred;
+			p->succ			= q->succ;
+			q->succ->pred	= p;
+			q->succ			= p;
+			p->pred			= q;
+			p = prod;
+		}
+	} while (p != prod->pred);
+
 	return product;
 }
 
@@ -234,6 +273,8 @@ void print_poly(poly_t* poly)
 	list_t* p = poly->terms;
 	if (p->term->coff == 1) {
 		printf("x");
+	} else if (p->term->coff == -1) {
+		printf("-x");
 	} else {
 		printf("%dx", p->term->coff);
 	}
@@ -250,7 +291,7 @@ void print_poly(poly_t* poly)
 				putchar('+');
 			}
 			putchar(' ');
-		if (p->term->exp != 0) {			
+		if (p->term->exp != 0) {
 			if (p->term->coff == 1) {
 				putchar('x');
 			} else {
